de072dc788ad4c3b4394cbec0ce667a6fccdde55
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2022-06-10 11:47:16 +0500

    debug 7

--- backend/app.js
+++ backend/app.js
@@ -11,15 +11,15 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('frontend/build'));
 
-app.get("*", (req, res) => {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/frontend/build/index.html`);
-});
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoryRouter);
 app.use("/api/requests", requestRouter);
 app.use("/api/orders", orderRouter);
+app.get("*", (req, res) => {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/frontend/build/index.html`);
+});
 
 module.exports = app;
