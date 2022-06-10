3c5f89083745cb89fce08160587c72d227f8e848
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2022-06-10 11:42:22 +0500

    debug 6

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('frontend/build'));
 
+app.get("*", (req, res) => {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/frontend/build/index.html`);
+});
+
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoryRouter);
